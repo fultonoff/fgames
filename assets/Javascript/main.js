18e73527896e522b369f469f0f9b9b14d124fbf3
fulton <fultonkilebe7@gmail.com>
2021-04-27 23:03:48 +0300

    why choose us

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -7,3 +7,20 @@ toogle.addEventListener('click', ()=>{
     nav.classList.toggle('active');
     toogle.classList.toggle('active');
 })
+
+
+var input = document.querySelector('.glide')
+
+var glide = new Glide('.glide', {
+  autoplay: 2000,
+  hoverpause: false,
+  perView: 1
+})
+
+input.addEventListener('input', function (event) {
+  glide.update({
+    autoplay: (event.target.value != 0) ? event.target.value : false
+  })
+})
+
+glide.mount()
