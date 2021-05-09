caaea9f082fb4486701d4439b8c2d88e45b17add
fulton <fultonkilebe7@gmail.com>
2021-05-09 19:04:51 +0300

    popup

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,33 +1,39 @@
-
-
 const nav = document.querySelector('.nav__list');
 const toogle = document.querySelector('.nav__toggle');
 
+function tog() {
+  nav.classList.toggle('active');
+  toogle.classList.toggle('active');
+}
 
-toogle.addEventListener('click', ()=>{
+toogle.addEventListener('click', () => {
+  tog();
+});
 
-    nav.classList.toggle('active');
-    toogle.classList.toggle('active');
-})
-
-
-var input = document.querySelector('.glide')
+var input = document.querySelector('.glide');
 
 var glide = new Glide('.glide', {
   autoplay: 2000,
   hoverpause: false,
-  perView: 1
-})
+  perView: 1,
+});
 
 input.addEventListener('input', function (event) {
   glide.update({
-    autoplay: (event.target.value != 0) ? event.target.value : false
-  })
-})
+    autoplay: event.target.value != 0 ? event.target.value : false,
+  });
+});
 
-glide.mount()
+glide.mount();
 
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    alt();
+  }, 3000);
+});
 
-
-
-
+function alt() {
+  alert(
+    'FOR SOME REASON GOOGLE CHROME DOESNT OPEN THE DOWNLOAD PAGE, PLEASE CHOOSE A DIFFERENT BROWSER'
+  );
+}
